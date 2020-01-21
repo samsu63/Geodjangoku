fe6bc8936b338ead9cd328a1534411c259fffd9a
Samsu Arif <samsuarif@gmail.com>
2020-01-21 16:53:37 +0800

    Program coba-coba

--- web_geo/web_geo/settings.py
+++ web_geo/web_geo/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'web_geo.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
